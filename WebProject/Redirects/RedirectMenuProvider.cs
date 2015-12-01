59bd8f8b915f34ada05dab6299d98f857b25481a
mikkohu <mikko.huilaja@solita.fi>
2015-12-01 15:16:27 +0200

    RedirectMenuProvider and edit navigation UI

---
+++ WebProject/Redirects/RedirectMenuProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using EPiServer.Security;
+using EPiServer.Shell.Navigation;
+
+namespace WebProject.Redirects
+{
+    [MenuProvider]
+    public class RedirectMenuProvider : IMenuProvider
+    {
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            return new MenuItem[] 
+            {
+                new UrlMenuItem("Redirect Manager", "/global/cms/redirects", "/Views/Admin/RedirectManager.cshtml")
+                {
+                    IsAvailable = context => PrincipalInfo.HasAdminAccess
+                }
+            };
+        }
+    }
+}
